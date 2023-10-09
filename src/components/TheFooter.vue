4c9f7f359e2ce06eb06bc33d56bd2ab706d7d4e5
Gustavo <122057283+coartem@users.noreply.github.com>
2023-10-09 18:19:28 -0300

    main page

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -11,27 +11,3 @@ async function toggleLocales() {
   locale.value = newLocale
 }
 </script>
-
-<template>
-  <nav flex="~ gap-4" mt-6 justify-center text-xl>
-    <RouterLink icon-btn to="/" :title="t('button.home')">
-      <div i-carbon-campsite />
-    </RouterLink>
-
-    <button icon-btn :title="t('button.toggle_dark')" @click="toggleDark()">
-      <div i="carbon-sun dark:carbon-moon" />
-    </button>
-
-    <a icon-btn :title="t('button.toggle_langs')" @click="toggleLocales()">
-      <div i-carbon-language />
-    </a>
-
-    <RouterLink icon-btn to="/about" :title="t('button.about')" data-test-id="about">
-      <div i-carbon-dicom-overlay />
-    </RouterLink>
-
-    <a icon-btn rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <div i-carbon-logo-github />
-    </a>
-  </nav>
-</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,26 +1,133 @@
-<script setup lang="ts">
-defineOptions({
-  name: 'IndexPage',
-})
-const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-
-const { t } = useI18n()
-</script>
-
 <template>
-  <div>
-    <p>teste</p>
-  </div>
+  <body class="relative font-sans text-black">
+    <div id="app" data-server-rendered="true" data-v-app>
+      <header data-v-14f5f3d7 class="header z-40">
+        <a
+          data-v-14f5f3d7 aria-current="page" href="/"
+          class="router-link-exact-active absolute m-5 h-12 w-12 select-none outline-none xl:fixed" focusable="false"
+        >
+          <svg data-v-6bb35677="" data-v-14f5f3d7="" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <title data-v-6bb35677="">Anthony Fu @ antfu.me</title>
+            <mask
+              id="mask0_634_3" data-v-6bb35677="" maskUnits="userSpaceOnUse" x="24" y="7" width="50" height="87"
+              style="mask-type: alpha;"
+            >
+              <path
+                data-v-6bb35677=""
+                d="M73.7892 9.12595C69.3501 1.20478 61.8262 17.7197 60.2461 21.1572C56.9356 28.6299 55.2305 35.5788 53.5 43.5C51.5438 41.4076 48.5048 36.8913 47 34.5C46 32.5 44.178 33.3348 45.2582 35.5C44.2048 33.7065 44 31 40.4582 28.8541C31.3543 28.0321 23.7209 43.6536 25 51.5C28.5029 62.7044 40 53 42.8659 47.9845C42.5649 50.8241 42.5649 54.7847 45.424 56.1298C48.5 57 49.8631 54.9342 51.8194 52.9913C50.6908 64.7236 49.2612 76.9042 53.1737 88.4123C54.9794 94.9884 61.3747 96.6324 62.0519 88.5618C62.9547 78.5482 60.4719 68.7589 57.4623 59.3431C56.6347 56.6529 55.5 54.5 54.6886 51.7956C54.6886 48 55.6585 50.6972 57.0861 52.244C58.7413 54.0375 64.309 59.3431 66.5 59.5C65 57.5 64.5348 56.8771 64.309 56.6529C61.9014 53.6638 59.0423 50.8989 56.4842 47.9845C61.6757 40.5117 66.7167 32.3663 70.1777 23.7726C71.6825 19.3637 74.8425 13.7591 73.7892 9.12595ZM43.3926 40.8853C41.7373 45.4437 39.7811 48.956 35.9439 52.0945C26.1502 60.1051 25.1428 47.741 28.6457 41.8568C33.16 32.4411 42.7154 24.0715 43.3926 40.8853ZM52.5 49C51.2962 50.7187 49.5314 53.3274 47.5 54C45.8447 54.4484 45.3488 53.2902 45.0478 51.7956C44.145 46.8636 45.725 42.0063 46.4021 37.2237C48.0574 40.3622 50.119 42.8614 52 46C52.3762 46.822 53.0267 48.1033 52.5 49ZM55.1299 56.1298C58.3652 65.9192 60.5471 76.1569 59.6442 86.3947C58.7413 95.362 55.8823 88.8607 54.7537 84.3023C52.4213 74.2887 53 63.5 54.227 53.8133C54.528 54.5606 54.9042 55.6067 55.1299 56.1298ZM71.4568 11.1436C69.8767 21.755 62.0519 36.7006 55.4308 45.0701C56.7851 38.0457 58.7413 31.2454 61.45 24.5946C61.6757 23.2495 71.6072 3.52135 71.4568 11.1436Z"
+                fill="white"
+              />
+            </mask>
+            <g data-v-6bb35677="" mask="url(#mask0_634_3)">
+              <path
+                data-v-6bb35677="" class="path1"
+                d="M43.8252 39.3048C43.5 29 38 27 31 35.5C16.9959 58.4158 35 61 42.5 46C44.4559 42.5 45.5001 37 45.5001 37C44.8789 42.2731 43.8252 45.9801 43.8252 50C43.8252 56.6285 48 57.9 54.5 48.4C60.5 40.9 68.5 27.5 72 15.5C74.4792 7 70.5 4.5 64 16.5C55 31.5 50.7595 59 52.3 78C53.8038 96.5469 63 97.5 61 79.5C59.5082 66.0734 52 42.5 45.5001 33C48 38.5 57 52 65.5 58.5"
+                stroke="black" stroke-width="2.5" stroke-linecap="round"
+              />
+            </g>
+          </svg>
+        </a>
+      </header>
+      <main class="of-x-hidden px-7 py-10">
+        <div
+          class="pointer-events-none fixed bottom-0 left-0 right-0 top-0 print:hidden"
+          style="z-index:-1; -webkit-mask-image: radial-gradient(circle, transparent, black)"
+        >
+          <canvas width="1920" height="983" style="width: 1920px; height: 983px;" />
+        </div>
+        <div class="m-auto mb-8 prose">
+          <h1 class="slide-enter-50 mb-0">
+            Gustavo Willian
+          </h1>
+        </div>
+        <article>
+          <div class="slide-enter-content m-auto prose">
+            <p>Hey, I am Anthony Fu, a fanatical open sourceror.</p>
+            <p>
+              Working at <a href="https://nuxtlabs.com/" target="_blank" rel="noopener"><span
+                i-simple-icons-nuxtdotjs=""
+              /> NuxtLabs</a>.<br> Creator of <a
+                href="https://github.com/vitest-dev/vitest" target="_blank" rel="noopener"
+              >Vitest</a>, <a
+                href="https://github.com/slidevjs/slidev" target="_blank" rel="noopener"
+              >Slidev</a>, <a
+                href="https://github.com/vueuse/vueuse" target="_blank" rel="noopener"
+              >VueUse</a>, <a
+                href="https://github.com/antfu/unocss" target="_blank" rel="noopener"
+              >UnoCSS</a>, <a
+                href="https://github.com/elk-zone/elk" target="_blank" rel="noopener"
+              >Elk</a>, <a
+                href="https://github.com/type-challenges/type-challenges" target="_blank" rel="noopener"
+              >Type
+                Challenges</a> and <a href="/projects">others</a>.<br> Core team member of <a
+                href="https://vuejs.org/"
+                target="_blank" rel="noopener"
+              >Vue</a>, <a
+                href="http://nuxtjs.org/" target="_blank"
+                rel="noopener"
+              >Nuxt</a> and <a href="http://vitejs.dev/" target="_blank" rel="noopener">Vite</a>.<br>
+            </p>
+            <p>
+              Dreaming up ideas and making them come true is where my passion lies. You can find my <a
+                href="/projects"
+              >full projects list here</a>. I also do some generative-art, compform, interactivity
+              experiments on <a href="https://100.antfu.me/" target="_blank" rel="noopener">100.antfu.me</a>.
+            </p>
+            <p>
+              Outside of programming, I enjoy doing photography and traveling. Some of my photos can be found on <a
+                href="https://www.instagram.com/antfu7" target="_blank" rel="noopener"
+              >Instagram</a>. If you happen to be
+              in the same city I live (currently in Paris), maybe we can hang out and take some photos or code together.
+            </p>
+            <div flex-auto="" />
+            <p>Find me on</p>
+            <p flex="~ gap-3 wrap" class="mt--2!">
+              <a href="https://github.com/antfu" target="_blank"><span op75="" i-simple-icons-github="" /> GitHub</a><a
+                href="https://elk.zone/m.webtoo.ls/@antfu" target="_blank"
+              ><span op75="" i-simple-icons-mastodon="" />
+                Mastodon</a><a href="https://www.twitter.com/antfu7" target="_blank"><span
+                op75=""
+                i-simple-icons-twitter=""
+              />
+                Twitter</a><a href="https://chat.antfu.me" target="_blank"><span op75="" i-simple-icons-discord="" />
+                Discord Server</a><a href="https://www.youtube.com/anthonyfu7" target="_blank"><span
+                op75=""
+                i-simple-icons-youtube=""
+              /> YouTube</a><a href="https://www.twitch.tv/antfu7" target="_blank"><span
+                op75="" i-simple-icons-twitch=""
+              /> Twitch</a><a
+                href="https://www.instagram.com/antfu7"
+                target="_blank"
+              ><span op75="" i-simple-icons-instagram="" />
+                Instagram</a><a href="https://www.zhihu.com/people/antfu" target="_blank"><span
+                op75=""
+                i-simple-icons-zhihu=""
+              /> 知乎</a><a href="https://weibo.com/u/7485197193" target="_blank"><span
+                op75=""
+                i-simple-icons-sinaweibo=""
+              /> 微博</a><a href="https://space.bilibili.com/668380" target="_blank"><span
+                op75="" i-simple-icons-bilibili=""
+              /> 哔哩哔哩</a>
+            </p>
+            <p>
+              If you enjoy my works, consider sponsoring me on <a
+                href="https://github.com/sponsors/antfu"
+                target="_blank" rel="noopener"
+              ><span i-carbon-favorite="" /> GitHub Sponsor</a> or <a
+                href="https://afdian.net/a/antfu" target="_blank" rel="noopener"
+              ><span i-carbon-lightning="" />
+                爱发电</a> to keep them sustainable.
+            </p>
+          </div>
+        </article>
+        <div class="slide-enter m-auto mb-6 mt-10 flex prose animate-delay-1200!">
+          <span class="text-sm op50"><a
+            target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color: inherit;"
+          >CC
+            BY-NC-SA 4.0</a> 2021-PRESENT © Anthony Fu</span>
+          <div class="flex-auto" />
+        </div>
+      </main>
+    </div>
+  </body>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: home
-</route>
