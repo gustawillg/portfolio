9e5d11456c6dea26559402f7a795d081c87b408b
Gustavo <122057283+coartem@users.noreply.github.com>
2023-11-01 15:37:47 -0300

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -25,25 +25,15 @@
 
           <!-- Mobile menu, show/hide based on menu state. -->
           <div id="mobile-menu" class="sm:hidden">
-            <div class="flex items-center md:hidden">
-              <button>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                  stroke="currentColor" class="h-6 w-6"
-                >
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-              </button>
-              <div class="px-2 pb-3 pt-2 space-y-1">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a
-                  href="#" class="block rounded-md px-4 py-4 text-sm font-medium text-black hover:text-orange"
-                  aria-current="page"
-                >Dashboard</a>
-                <a href="#" class="block rounded-md px-4 py-4 text-sm font-medium text-black hover:text-orange">Team</a>
-                <a href="#" class="block rounded-md px-4 py-4 text-sm font-medium text-black hover:text-orange">Projects</a>
-                <a href="#" class="block rounded-md px-4 py-4 text-sm font-medium text-black hover:text-orange">Calendar</a>
-              </div>
+            <div class="px-2 pb-3 pt-2 space-y-1">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <a
+                href="#" class="block rounded-md px-4 py-4 text-sm font-medium text-black hover:text-orange"
+                aria-current="page"
+              >Dashboard</a>
+              <a href="#" class="block rounded-md px-4 py-4 text-sm font-medium text-black hover:text-orange">Team</a>
+              <a href="#" class="block rounded-md px-4 py-4 text-sm font-medium text-black hover:text-orange">Projects</a>
+              <a href="#" class="block rounded-md px-4 py-4 text-sm font-medium text-black hover:text-orange">Calendar</a>
             </div>
           </div>
         </nav>
